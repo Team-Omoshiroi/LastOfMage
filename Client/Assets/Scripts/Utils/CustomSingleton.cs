b111048318293371857af8b2dbbcbcd9fc686a04
cutlake2000 <cutlake2000@naver.com>
2023-11-21 22:48:19 +0900

    #287 에러 수정 중

--- Client/Assets/Scripts/Utils/CustomSingleton.cs
+++ Client/Assets/Scripts/Utils/CustomSingleton.cs
@@ -19,11 +19,14 @@ public class CustomSingleton<T> : MonoBehaviour where T : MonoBehaviour
             {
                 m_Instance = (T)FindObjectOfType(typeof(T));
 
+                GameObject singletonObject = new() { name = "@" + typeof(T).ToString() };
+
                 if (m_Instance == null)
                 {
-                    GameObject singletonObject = new GameObject { name = "@" + typeof(T).ToString() };
                     m_Instance = singletonObject.AddComponent<T>();
-
+                }
+                else
+                {
                     DontDestroyOnLoad(singletonObject);
                 }
             }
